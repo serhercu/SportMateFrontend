b71eb6e4a14c54e7747bf61f90bac6c61811c024
Sergio Herrando Cuenca <herrandosergio@gmail.com>
2022-05-17 20:00:10 +0200

    Menú

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import MenuIcon from 'vue-material-design-icons/Menu.vue';
 import i18n from './i18n'
 
 Vue.config.productionTip = false
@@ -11,6 +12,7 @@ new Vue({
   router,
   store,
   vuetify,
+  MenuIcon,
   i18n,
   render: h => h(App)
 }).$mount('#app')
