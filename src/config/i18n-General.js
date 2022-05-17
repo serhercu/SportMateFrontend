d750b9a72bb317d05e3e27ea3fe8dc076f2abaf2
Sergio Herrando Cuenca <herrandosergio@gmail.com>
2022-05-17 20:57:08 +0200

    i18 funcionando

---
+++ src/config/i18n-General.js
@@ -0,0 +1,20 @@
+const messagesi18 = {
+    es: {
+      createGame: {
+          sport: 'Deporte',
+          dateAndLocation: 'Fecha y lugar',
+          numberOfPlayers: 'Número de jugadores',
+          description: 'Descripción'
+      }
+    },
+    en: {
+      createGame: {
+        sport: 'Sport',
+        dateAndLocation: 'Date and Location',
+        numberOfPlayers: 'Number of players',
+        description: 'Description'
+    }}
+  }
+  
+  export default messagesi18
+  
--- src/main.js
+++ src/main.js
@@ -3,14 +3,23 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
-import MenuIcon from 'vue-material-design-icons/Menu.vue'
+import VueI18n from 'vue-i18n'
+
+import messagesi18 from '@/config/i18n-General'
 
 Vue.config.productionTip = false
+
+Vue.use(VueI18n)
+
+const i18n = new VueI18n({
+  locale: 'es', // set locale
+  messages: messagesi18 // set locale messages
+})
 
 new Vue({
   router,
   store,
   vuetify,
-  MenuIcon,
+  i18n,
   render: h => h(App)
 }).$mount('#app')
