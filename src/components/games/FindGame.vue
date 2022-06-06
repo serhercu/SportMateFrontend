617a8d8a3b055855f1b042b37fe26077145c9bb3
Sergio Herrando Cuenca <herrandosergio@gmail.com>
2022-06-06 21:30:03 +0200

    Buscar partida

---
+++ src/components/games/FindGame.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-layout>
+    <v-container fluid>
+      <v-card class="ma-8 pa-6 amber" fluid>
+        <v-row>
+          <p class="d-flex justify-center">Buscar partida</p>
+        </v-row>
+        <v-row>
+          <v-select background-color="white" label="gola"></v-select>
+        </v-row>
+      </v-card>
+    </v-container>
+  </v-layout>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 import CreateGame from '@/components/games/CreateGame'
+import FindGame from '@/components/games/FindGame'
 
 Vue.use(VueRouter)
 
@@ -11,6 +12,11 @@ const routes = [
     name: 'createGame',
     component: CreateGame
   },
+  {
+    path: '/findGame',
+    name: 'findGame',
+    component: FindGame
+  }
 ]
 
 const router = new VueRouter({
